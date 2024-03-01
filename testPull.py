b19275cac4fc371cad405ace336d53af15d99025
Zanger67 <anthonyzang101@gmail.com>
2024-03-01 05:46:55 -0500

    finsihed cosine similarity value request via terminal

--- testPull.py
+++ testPull.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import numpy as np
 
 
 
@@ -9,19 +10,55 @@ def getBaseUrl() :
 def getVectorUrl(wordGuess : str, actualWord : str) : # word guess
     return getBaseUrl() + 'model2/' + actualWord + '/' + wordGuess
 
-def getActualUrl(actualWord : str) :
-    return getVectorUrl(actualWord, actualWord)
+def getActualUrl(actualWord : str) : # actual word to comapare
+    # if actualUrl == '' :
+    actualUrl = getVectorUrl(actualWord, actualWord)
 
-urlRequest = semantleStarterRequest + 'plane'
+    return actualUrl
+
+def getCosineSimilarity(wordGuess : str, actualWord : str) :
+    if wordGuess in previousCases :
+        return previousCases[wordGuess]
+    
+    try :
+        guessJson = requests.get(getVectorUrl(wordGuess, actualWord)).json()
+    except :
+        return -1.0
+
+    # print(guessJson)
+    # if not guessJson['vec'] :
+    #     return 0
+    
+    temp = np.dot(guessJson['vec'], actualJson['vec']) / (np.linalg.norm(guessJson['vec']) * np.linalg.norm(actualJson['vec']))
+
+    previousCases[wordGuess] = temp
+
+    return temp
 
 
-initialJson = requests.get(urlRequest).json()
 
-print(initialJson,'\n\n\n\n')
+todaysWord = str(input('Enter the actual word: '))
+actualUrl = getActualUrl(todaysWord)
+actualJson = requests.get(actualUrl).json()
+previousCases = {}
 
-testRequest = pd.DataFrame(initialJson)
+while True :
+    guess = str(input('Enter your guess: '))
+    if guess == 'exit' :
+        break
+    print(getCosineSimilarity(guess, todaysWord))
 
-print(testRequest)
 
-# print('sum\t', sum((testRequest.values)))
-print()
+
+
+
+# initialJson = requests.get(urlRequest).json()
+
+# print(initialJson,'\n\n\n\n')
+
+# testRequest = pd.DataFrame(initialJson)
+
+# print(testRequest)
+
+# # print('sum\t', sum((testRequest.values)))
+# print()
